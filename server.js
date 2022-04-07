1644bc46f47650fa0dafef772715c1a7c5bb6116
RekhaLeelara <62070645+RekhaLeelara@users.noreply.github.com>
2022-04-06 23:52:30 -0500

    updated the user login or signup

--- server.js
+++ server.js
@@ -2,103 +2,54 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
 const path = require('path');
-const User = require('./models/User');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var morgan = require('morgan');
-
+const sequelize = require('./config/connection');
 const hbs = exphbs.create({});
-
+const session = require('express-session');
+const routes = require('./controllers/user-routes');
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3002;
+// const routes = require(path.join(__dirname, 'controllers'));
+const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const sess = {
+  secret: 'bigbluedog',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
+
+
+app.use(session(sess));
 
 // Set Handlebars as the default template engine.
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(require('./controllers/user-routes'));
+app.use(require('./controllers/patient-routes.js'));
+app.use(require('./controllers/doctor-routes.js'));
+app.use(require('./controllers/login-routes.js'));
+app.use(require('./controllers/home-routes.js'));
+app.use(require('./controllers/book-appointment.js'));
+app.use(express.static('views/assets/img'));
 
-
-// set morgan to log info about our requests for development use.
-app.use(morgan('dev'));
-
-// initialize body-parser to parse incoming parameters requests to req.body
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// initialize cookie-parser to allow us access the cookies stored in the browser. 
-app.use(cookieParser());
-
-// initialize express-session to allow us track the logged-in user across sessions.
-app.use(session({
-    key: 'user_sid',
-    secret: 'somerandonstuffs',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        expires: 600000
-    }
-}));
-
-// handle bars config
-// app.engine('hbs', hbs({extname: 'hbs',defaultLayout: 'layout', layoutsDir: __dirname + '/views/layouts/'})); 
-// app.set('views', path.join(__dirname, 'views')); 
-// app.set('view engine', 'hbs'); 
-// This middleware will check if user's cookie is still saved in browser and user is not set, then automatically log the user out.
-// This usually happens when you stop your express server after login, your cookie still remains saved in the browser.
-app.use((req, res, next) => {
-    if (req.cookies.user_sid && !req.session.user) {
-        res.clearCookie('user_sid');        
-    }
-    next();
-});
-
-var hbsContent = {userName: '', loggedin: false, title: "You are not logged in today", body: "Hello World"}; 
-
-// middleware function to check for logged-in users
-var sessionChecker = (req, res, next) => {
-    if (req.session.user && req.cookies.user_sid) {
-		
-        res.redirect('/dashboard');
-    } else {
-        next();
-    }    
-};
-
-
-// route for Home-Page
-app.get('/', sessionChecker, (req, res) => {
-    res.redirect('/login');
-});
-
-
-// // route for user signup
-// app.route('/user')
-//     //.get(sessionChecker, (req, res) => {
-//     .get((req, res) => {
-//         //res.sendFile(__dirname + '/public/signup.html');
-//         res.render('user', hbsContent);
-//     })
-//     .post((req, res) => {
-//       console.log("inside post request");
-//       User.create({
-//             username: req.body.username,
-//             //email: req.body.email,
-//             password: req.body.password
-//         })
-//         .then(user => {
-//             req.session.user = user.dataValues;
-//             // res.redirect('/dashboard');
-//         })
-//         .catch(error => {
-//           console.log("Data not stored in the database");
-//             // res.redirect('/user');
-//         });
-//     });
-
+app.use(routes);
 // Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log(`Server listening on: http://localhost:${PORT}`);
+// const server = require("http").Server(app);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
+
